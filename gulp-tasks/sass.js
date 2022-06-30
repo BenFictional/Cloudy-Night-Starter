83895eb6511c7117e3fa652c69f2acd6e882fa20
BenFictional <beandrew@gmail.com>
2022-06-30 15:37:51 -0400

    Fixing gulp version bug

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -1,6 +1,6 @@
 const { dest, src } = require('gulp');
 const cleanCSS = require('gulp-clean-css');
-const sassProcessor = require('gulp-sass');
+const sassProcessor = require('gulp-sass')(require('sass'));
 
 // We want to be using canonical Sass, rather than node-sass
 sassProcessor.compiler = require('sass');
